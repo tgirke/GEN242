1a6df3d1967c1e7b3da9ec4ba92452e8310977b0
tgirke <thomas.girke@ucr.edu>
2022-04-06 16:29:24 -0700

    some edits

--- docs/custom/slides/R_for_HPC/demo_files/my_script.R
+++
@@ -1,9 +0,0 @@
-##########################
-## Some simple R script ##
-##########################
-
-## Create some node specific data and write to file
-Node <- system("hostname", intern=TRUE)
-Time <- date()
-writeLines(c(Node, Time), "test_output_alphabet.txt")
-
