cfe0a4a4e117b2e467c1798fff199a8adad8fbea
tgirke <tgirke@citrus.ucr.edu>
2022-05-22 13:12:25 -0700

    some edits

--- docs/custom/slides/rpackages/rpackages_slides.md
+++ docs/custom/slides/rpackages/rpackages_slides.md
@@ -1,7 +1,7 @@
 ---
 title: "Building R Packages"
 author: Thomas Girke
-date: May 25, 2021
+date: May 24, 2022
 output: 
   ioslides_presentation:
     keep_md: yes
@@ -335,31 +335,24 @@ sessionInfo()
 ```
 
 ```
-## R version 4.1.0 (2021-05-18)
+## R version 4.2.0 (2022-04-22)
 ## Platform: x86_64-pc-linux-gnu (64-bit)
-## Running under: Debian GNU/Linux 10 (buster)
+## Running under: Debian GNU/Linux 11 (bullseye)
 ## 
 ## Matrix products: default
-## BLAS:   /usr/lib/x86_64-linux-gnu/blas/libblas.so.3.8.0
-## LAPACK: /usr/lib/x86_64-linux-gnu/lapack/liblapack.so.3.8.0
+## BLAS:   /usr/lib/x86_64-linux-gnu/blas/libblas.so.3.9.0
+## LAPACK: /usr/lib/x86_64-linux-gnu/lapack/liblapack.so.3.9.0
 ## 
 ## locale:
-##  [1] LC_CTYPE=en_US.UTF-8       LC_NUMERIC=C              
-##  [3] LC_TIME=en_US.UTF-8        LC_COLLATE=en_US.UTF-8    
-##  [5] LC_MONETARY=en_US.UTF-8    LC_MESSAGES=en_US.UTF-8   
-##  [7] LC_PAPER=en_US.UTF-8       LC_NAME=C                 
-##  [9] LC_ADDRESS=C               LC_TELEPHONE=C            
-## [11] LC_MEASUREMENT=en_US.UTF-8 LC_IDENTIFICATION=C       
+##  [1] LC_CTYPE=en_US.UTF-8       LC_NUMERIC=C               LC_TIME=en_US.UTF-8        LC_COLLATE=en_US.UTF-8     LC_MONETARY=en_US.UTF-8    LC_MESSAGES=en_US.UTF-8    LC_PAPER=en_US.UTF-8       LC_NAME=C                 
+##  [9] LC_ADDRESS=C               LC_TELEPHONE=C             LC_MEASUREMENT=en_US.UTF-8 LC_IDENTIFICATION=C       
 ## 
 ## attached base packages:
 ## [1] stats     graphics  grDevices utils     datasets  methods   base     
 ## 
 ## loaded via a namespace (and not attached):
-##  [1] digest_0.6.27     R6_2.5.0          jsonlite_1.7.2    magrittr_2.0.1   
-##  [5] evaluate_0.14     rlang_0.4.11      stringi_1.6.2     jquerylib_0.1.4  
-##  [9] bslib_0.2.5.1     rmarkdown_2.8     tools_4.1.0       stringr_1.4.0    
-## [13] xfun_0.23         yaml_2.2.1        compiler_4.1.0    htmltools_0.5.1.1
-## [17] knitr_1.33        sass_0.4.0
+##  [1] digest_0.6.29   R6_2.5.1        jsonlite_1.8.0  magrittr_2.0.3  evaluate_0.15   stringi_1.7.6   rlang_1.0.2     cli_3.3.0       jquerylib_0.1.4 bslib_0.3.1     rmarkdown_2.14  tools_4.2.0     stringr_1.4.0  
+## [14] xfun_0.30       yaml_2.3.5      fastmap_1.1.0   compiler_4.2.0  htmltools_0.5.2 knitr_1.39      sass_0.4.1
 ```
 
 ## References
--- static/custom/slides/rpackages/rpackages_slides.R
+++ static/custom/slides/rpackages/rpackages_slides.R
@@ -7,6 +7,7 @@
 ## }
 
 ## 
+
 ## pre[class] {
 
 ##   max-height: 300px;
@@ -25,49 +26,49 @@
 ## }
 
 
-## ----package_skeleton1, eval=FALSE--------------------------------------------
+## ----package_skeleton1, eval=FALSE----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 ## ## Download R script (here pkg_build_fct.R) containing two sample functions
 ## download.file("https://raw.githubusercontent.com/tgirke/GEN242/main/content/en/tutorials/rpackages/helper_functions/pkg_build_fct.R", "pkg_build_fct.R")
 ## ## Build package skeleton based on functions in pkg_build_fct.R
 ## package.skeleton(name="mypackage", code_files=c("pkg_build_fct.R"))
 
 
-## ----r_build_package, eval=FALSE----------------------------------------------
+## ----r_build_package, eval=FALSE------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 ## system("R CMD build mypackage")
 ## system("R CMD check mypackage_1.0.tar.gz")
 
 
-## ----install_package, eval=FALSE----------------------------------------------
+## ----install_package, eval=FALSE------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 ## install.packages("mypackage_1.0.tar.gz", repos=NULL)
 
 
-## ----package_devtools_skeleton1, eval=FALSE-----------------------------------
+## ----package_devtools_skeleton1, eval=FALSE-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 ## library("devtools"); library("roxygen2"); library("usethis"); library(sinew) # If not availble install these packages with 'install.packages(...)'
 ## create("myfirstpkg") # Creates package skeleton. The chosen name (here myfirstpkg) will be the name of the package.
 ## setwd("myfirstpkg") # Set working directory of R session to package directory 'myfirstpkg'
 ## use_mit_license() # Add license information to description file (here MIT). To look up alternatives, do ?use_mit_license
 
 
-## ----package_devtools_skeleton2, eval=FALSE-----------------------------------
+## ----package_devtools_skeleton2, eval=FALSE-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 ## download.file("https://raw.githubusercontent.com/tgirke/GEN242/main/content/en/tutorials/rpackages/helper_functions/pkg_build_fct.R", "R/pkg_build_fct.R")
 
 
-## ----package_devtools_skeleton3, eval=FALSE-----------------------------------
+## ----package_devtools_skeleton3, eval=FALSE-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 ## load_all() # Loads package in a simulated way without installing it.
 ## writeLines(makeOxygen(myMAcomp), "myroxylines") # This creates a 'myroxylines' file in current directory. Delete this file after adding its content to the corresponding functions.
 
 
-## ----package_devtools_skeleton4, eval=FALSE-----------------------------------
+## ----package_devtools_skeleton4, eval=FALSE-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 ## document() # Auto-generates/updates *.Rd files under man directory (here: myMAcomp.Rd and talkToMe.Rd)
 ## tools::Rd2txt("man/myMAcomp.Rd") # Renders Rd file from source
 ## tools::checkRd("man/myMAcomp.Rd") # Checks Rd file for problems
 
 
-## ----package_devtools_skeleton5, eval=FALSE-----------------------------------
+## ----package_devtools_skeleton5, eval=FALSE-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 ## use_vignette("introduction", title="Introduction to this package")
 
 
-## ----package_devtools_skeleton6, eval=FALSE-----------------------------------
+## ----package_devtools_skeleton6, eval=FALSE-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 ## setwd("..") # Redirect R session to parent directory
 ## check("myfirstpkg") # Check package for problems, when in pkg dir one can just use check()
 ## # remove.packages("myfirstpkg") # Optional. Removes test package if already installed
@@ -75,17 +76,17 @@
 ## build("myfirstpkg") # Creates *.tar.gz file for package required to for submission to CRAN/Bioc
 
 
-## ----package_devtools_skeleton7, eval=FALSE-----------------------------------
+## ----package_devtools_skeleton7, eval=FALSE-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 ## library("myfirstpkg")
 ## library(help="myfirstpkg")
 ## ?myMAcomp
 ## vignette("introduction", "myfirstpkg")
 
 
-## ----package_devtools_skeleton8, eval=FALSE-----------------------------------
+## ----package_devtools_skeleton8, eval=FALSE-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 ## devtools::install_github("<github_user_name>/<mypkg_repos>", subdir="myfirstpkg") # If the package is in the root directory of the repos, then the 'subdir' argument can be dropped.
 
 
-## ----sessionInfo--------------------------------------------------------------
+## ----sessionInfo----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 sessionInfo()
 
