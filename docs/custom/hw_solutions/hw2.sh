d502c3eed2b68374a32ea1bad5ce5c299ed7572e
tgirke <tgirke@citrus.ucr.edu>
2023-04-19 19:48:16 -0700

    some edits

--- docs/custom/hw_solutions/hw2.sh
+++ docs/custom/hw_solutions/hw2.sh
@@ -4,7 +4,7 @@
 ## HW02: Linux Basics Using Bioinformatics Examples ##
 ######################################################
 ## Author: First Last Name
-## Last update: 06-Apr-2022
+## Last update: 13-Apr-2023
 
 ## Download E. coli (Halobacterium) proteome and inspect it
 wget https://ftp.ncbi.nlm.nih.gov/genomes/genbank/bacteria/Escherichia_coli/reference/GCA_000008865.2_ASM886v2/GCA_000008865.2_ASM886v2_protein.faa.gz 
@@ -41,6 +41,6 @@ blastp -query myseq.fasta -db ecoli.faa -outfmt 6 -evalue 1e-6 -out blastp.tab
 ## Return system time and host name
 date 
 hostname 
-# Answer: Apr 06 16:25:58 PDT 2022
+# Answer: Apr 19 16:25:58 PDT 2022
 # Answer: parrot or some other host name of a node on HPCC
 
