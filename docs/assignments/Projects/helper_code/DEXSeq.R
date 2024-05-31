f28149df2a0f1ed6460d8a4813715a023ad6ffe8
tgirke <tgirke@citrus.ucr.edu>
2024-05-31 15:19:36 -0700

    some edits

--- docs/assignments/Projects/helper_code/DEXSeq.R
+++ docs/assignments/Projects/helper_code/DEXSeq.R
@@ -1,5 +1,5 @@
 #####################################
-## Run DESseq on Howard et al Data ##
+## Run DEXseq on Howard et al Data ##
 #####################################
 ## Helper code (May 25, 2024)
 
@@ -10,69 +10,93 @@ txdb <- loadDb("./data/tair10.sqlite")
 flattenedAnnotation2 <- exonicParts(txdb, linked.to.single.gene.only=TRUE)
 names(flattenedAnnotation2) <- sprintf("%s:E%0.3d", flattenedAnnotation2$gene_id, flattenedAnnotation2$exonic_part)
 
-## Exon-level read counting in parallel mode 
+## Exon-level read counting in serial mode 
 library(Rsamtools)
 library(GenomicAlignments)
+targets <- read.delim("targetsPE.txt", comment.char="#")
 outpaths <- list.files('results/hisat2_mapping', pattern='sorted.bam$', full.names=TRUE)
+outpaths <- setNames(outpaths, gsub("^.*mapping/(.*)\\.sorted.bam", "\\1", outpaths))
+outpaths <- outpaths[targets$SampleName] # This ensures proper colmun order
 file.exists(outpaths) # should be all true
 bfl <- BamFileList(outpaths)
 se2 <- summarizeOverlaps(
     flattenedAnnotation2, BamFileList(bfl), singleEnd=FALSE,
-    fragments=TRUE, ignore.strand=TRUE )
+    fragments=TRUE, ignore.strand=TRUE)
 assays(se2)$counts[1:4,]
 
-## Exon-level read counting in parallel mode 
-library(Rsamtools); library(GenomicAlignments); library(GenomicFeatures); library(BiocParallel)
-outpaths <- list.files('results/hisat2_mapping', pattern='sorted.bam$', full.names=TRUE)
-bfl <- BamFileList(outpaths, yieldSize = 50000, index = character())
-multicoreParam <- MulticoreParam(workers = 4)
-register(multicoreParam)
-registered()
-countExons <- bplapply(bfl, function(x) summarizeOverlaps(flattenedAnnotation2,
-        x, ignore.strand = TRUE, singleEnd = FALSE, BPPARAM = multicoreParam))
-countDFexons <- sapply(seq(along = countExons), function(x) assays(countExons[[x]])$counts)
-rownames(countDFexons) <- names(rowRanges(countExons[[1]]))
-colnames(countDFexons) <- names(bfl)
-countDFexons[1:4,]
-write.table(countDFexons, "results/countExons.xls", col.names = NA, quote = FALSE, sep = "\t")
-countDF <- read.delim("results/countExons.xls", row.names = 1, check.names = FALSE)
-
 ## Building DEXSeqDataSet object
-
 library(DEXSeq)
-targets <- read.delim("targetsPE.txt", comment.char="#")
 colData(se2)$condition <- factor(targets$Factor)
 colData(se2)$libType <- factor(c("paired-end"))
 dxd2 <- DEXSeqDataSetFromSE(se2, design= ~ sample + exon + condition:exon)
 assays(dxd2)$counts[1:4,]
 colData(dxd2)
 
+## Exon-level read counting in parallel mode 
+library(Rsamtools); library(GenomicAlignments); library(GenomicFeatures); library(BiocParallel)
+targets <- read.delim("targetsPE.txt", comment.char="#")
+outpaths <- list.files('results/hisat2_mapping', pattern='sorted.bam$', full.names=TRUE)
+outpaths <- setNames(outpaths, gsub("^.*mapping/(.*)\\.sorted.bam", "\\1", outpaths))
+outpaths <- outpaths[targets$SampleName] # This ensures proper colmun order
+file.exists(outpaths) # should be all true
+bfl <- BamFileList(outpaths, yieldSize = 50000, index = character())
+multicoreParam <- MulticoreParam(workers = 4)
+register(multicoreParam)
+registered()
+countExons <- bplapply(bfl, function(x) summarizeOverlaps(flattenedAnnotation2,
+        x, fragments=TRUE, ignore.strand = TRUE, singleEnd = FALSE, BPPARAM = multicoreParam))
+countDFexons <- sapply(seq(along = countExons), function(x) assays(countExons[[x]])$counts)
+rownames(countDFexons) <- names(rowRanges(countExons[[1]]))
+colnames(countDFexons) <- names(bfl)
+countDFexons[1:4,]
+write.table(countDFexons, "results/countExons.xls", col.names = NA, quote = FALSE, sep = "\t")
+
+## Building DEXSeqDataSet object from imported count file (coundDF)
+library(DEXSeq)
+countDF <- read.delim("results/countExons.xls", row.names = 1, check.names = FALSE)
+countDF <- SummarizedExperiment(assays=list(counts=countDF), rowRanges=flattenedAnnotation2)
+assays(countDF)$counts[1:4,]
+colData(countDF)
+targets <- read.delim("targetsPE.txt", comment.char="#")
+colData(countDF)$condition <- factor(targets$Factor)
+colData(countDF)$libType <- factor(c("paired-end"))
+dxd3 <- DEXSeqDataSetFromSE(countDF, design= ~ sample + exon + condition:exon)
+assays(dxd3)$counts[1:4,]
+colData(dxd3)
+
 ## Note that the number of columns is now doubled from 18 to 36. The first 18 
 ## correspond to the number of reads mapping to each exon and the last 18 correspond 
 ## to the sum of the counts mapping to the rest of the exons from the same gene in 
 ## each sample. To check, run:
-split( seq_len(ncol(dxd2)), colData(dxd2)$exon )
+split( seq_len(ncol(dxd3)), colData(dxd2)$exon )
 
 ## To show only the counts for the actual sample, run: 
-featureCounts(dxd2)[1:4,]
+featureCounts(dxd3)[1:4,]
 
 ## The exon range information can be returned as follows:
-rowRanges(dxd2)[1:4,]
+rowRanges(dxd3)[1:4,]
 
 ## The experimental design table can be returned like this:
-sampleAnnotation(dxd2)
+sampleAnnotation(dxd3)
 
 ## Normalisation
 
-dxd2 <- estimateSizeFactors( dxd2 )
+dxd3 <- estimateSizeFactors( dxd3 )
 
 ## Dispersion estimation
-dxd2 <- estimateDispersions( dxd2 )
-plotDispEsts( dxd2 )
+dxd3 <- estimateDispersions( dxd3 )
+plotDispEsts( dxd3 )
 ```
 
 ## Testing for differential exon usage
-dxd2 <- testForDEU( dxd2 )
+dxd3 <- testForDEU( dxd3 )
+dxd3 <- estimateExonFoldChanges( dxd3, fitExpToVar="condition")
+dxr4 <- DEXSeqResults( dxd3 )
+dxr4
+mcols(dxr4)$description
+
+table ( dxr4$padj < 0.1 )
+table ( tapply( dxr4$padj < 0.1, dxr4$groupID, any ) )
 
 ##################################
 ## Continue in DEXseq vignette ###
